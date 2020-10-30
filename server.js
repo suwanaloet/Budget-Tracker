f597d5273734a26ff987c9ecd35a96a5ce56f12a
Sean Suwanaloet <suwanaloet@wisc.edu>
2020-10-29 17:57:53 -0700

    added heroku links in server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+// https://fast-stream-32754.herokuapp.com/ | https://git.heroku.com/fast-stream-32754.git
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
