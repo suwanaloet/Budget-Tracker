16ef377decbac9536971878f7ebf6365dc50af12
Sean Suwanaloet <suwanaloet@wisc.edu>
2020-10-29 18:16:36 -0700

    deploy

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
